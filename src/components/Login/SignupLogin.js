70e89e45547afb8cd7609099ee0259b008daed02
sanjith-s <sanjithsenthilmurugan@gmail.com>
2023-09-16 14:50:48 +0530

    Done login Integration

--- src/components/Login/SignupLogin.js
+++ src/components/Login/SignupLogin.js
@@ -14,15 +14,62 @@ import {
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 import { Helmet } from "react-helmet";
+import Axios from "axios";
+import Swal  from "sweetalert2";
+import Cookies from 'js-cookie';
 
 const SignupLogin = () => {
   const [signUp, setSignUp] = useState(false);
+  const [credentials, setCredentials] = useState({ email: "", password: "" });9
   const signUpBtn = () => {
     setSignUp(true);
   };
   const loginBtn = () => {
     setSignUp(false);
   };
+
+  const handleChange = (e) => {
+    if (!signUp) {
+      let field = e.target.name;
+      let value = e.target.value;
+      credentials[field] = value;
+      setCredentials(credentials);
+    }
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+    let baseURL = "http://localhost:5000";
+    let url = "http://localhost:5000/api/admin/login"
+    let utype = "admin";
+
+    try {
+      // console.log(credentials)
+      let response = await Axios.post(
+        `${baseURL}/api/${utype}/login`,
+        credentials
+      );
+
+      // console.log(response)
+      if (response.status == 200) {
+        Swal.fire({
+          icon: "success",
+          title: "Login successful",
+        });
+
+        Cookies.set("token", response.data.token, {expires: 1})
+        localStorage.setItem("token", response.data.token)
+      } 
+    } catch (err) {
+      // console.log(err)
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: err.response.data.message
+      });
+    }
+  };
+
   return (
     <div className={`modalcontainer${signUp ? " sign-up-mode" : ""}`}>
       <div className="forms-container">
@@ -31,13 +78,23 @@ const SignupLogin = () => {
             <h2 className="formtitle">Login</h2>
             <div className="input-field">
               <FontAwesomeIcon className="profile" icon={faEnvelope} />
-              <input type="email" placeholder="Email" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+              />
             </div>
             <div className="input-field">
               <FontAwesomeIcon className="profile" icon={faLock} />
-              <input type="password" placeholder="Password" />
+              <input type="password" name="password" placeholder="Password" onChange={handleChange} />
             </div>
-            <input type="submit" value="Login" className="modalbtn solid" />
+            <input
+              type="submit"
+              value="Login"
+              className="modalbtn solid"
+              onClick={login}
+            />
             {/* <p className="social-text">Or login with social platforms</p>
             <div className="social-media">
               <a href="#" className="social-icon">
